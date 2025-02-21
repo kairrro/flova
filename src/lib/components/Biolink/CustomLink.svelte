365c2779c39c9d55ef9255a2954d51ebce158895
kairrro <damnkyro@proton.me>
2025-02-21 10:53:58 -0600

    fixed more coloring

--- src/lib/components/Biolink/CustomLink.svelte
+++ src/lib/components/Biolink/CustomLink.svelte
@@ -6,8 +6,9 @@
 
 <a
     {href}
-    class="flex items-center justify-center gap-2 text-sm p-2 py-[16px] rounded-xl bg-black/50 w-full transition hover:opacity-70 mt-2 text-[{text_color}]"
+    class="flex items-center justify-center gap-2 text-sm p-2 py-[16px] rounded-xl bg-black/50 w-full transition hover:opacity-70 mt-2"
     target="_blank"
+    style="color: {text_color}" 
 >   
     <p>{name}</p>
 </a>
